acd927e8ca7d1fe94fb6c0f3f7ec5e87faef4241
Ian Beauregard <ian.beauregard@gmail.com>
2021-03-20 16:48:24 -0400

    Destructor for Map

--- map.c
+++ map.c
@@ -1,11 +1,21 @@
 #include "map.h"
 #include <stdlib.h>
 #include <fcntl.h>
+#include <unistd.h>
+
+static void delete(Map* self);
 
 Map* new_map_from_path(const char* map_path)
 {
     Map* map = malloc(sizeof (Map));
-    map->fd = open(map_path, O_RDONLY);
 
+    map->fd = open(map_path, O_RDONLY);
+    map->delete = &delete;
     return map;
+}
+
+void delete(Map* self)
+{
+    close(self->fd);
+    free(self);
 }
