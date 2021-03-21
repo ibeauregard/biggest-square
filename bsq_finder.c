39e204342628dc887a355e2f0994e99ddd72fc97
Ian Beauregard <ian.beauregard@gmail.com>
2021-03-21 10:11:18 -0400

    Add basic destructor for BsqFinder

--- bsq_finder.c
+++ bsq_finder.c
@@ -1,11 +1,21 @@
 #include "bsq_finder.h"
 #include <stdlib.h>
 
+static void delete(BsqFinder* self);
+
 BsqFinder* new_bsq_finder(Map* map)
 {
     BsqFinder* self = malloc(sizeof (BsqFinder));
     self->map = map;
     self->row_index = 0;
 
+    self->delete = &delete;
+
     return self;
+}
+
+void delete(BsqFinder* self)
+{
+    free(self->sizes);
+    free(self);
 }
