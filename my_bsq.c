6b31793ef9fbcac32a4683f5a708bfc322b25a86
Ian Beauregard <ian.beauregard@gmail.com>
2021-03-21 10:42:23 -0400

    Correct small typo

--- my_bsq.c
+++ my_bsq.c
@@ -4,7 +4,7 @@
 
 int my_bsq(const char* map_path)
 {
-    Map *map = new_map_from_path(map_path);
+    Map* map = new_map_from_path(map_path);
     map->identifyBiggestSquare(map);
     map->print(map);
     map->delete(map);
