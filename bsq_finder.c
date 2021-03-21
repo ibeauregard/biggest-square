028ad6c221404d35a682c815a00cd0f1188105a4
Ian Beauregard <ian.beauregard@gmail.com>
2021-03-21 11:05:14 -0400

    Refactor

--- bsq_finder.c
+++ bsq_finder.c
@@ -20,23 +20,32 @@ BsqFinder* new_bsq_finder(Map* map)
     return self;
 }
 
+static void initialize_size_tracker(BsqFinder* self);
 static void handle_row(BsqFinder* self, char* row);
 
 BiggestSquare* run(BsqFinder* self)
 {
     char* row = self->map->getNextRow(self->map);
     self->map->nums_cols = _strlen(row);
-    self->size_tracker = malloc((self->map->nums_cols + 1) * sizeof (uint));
-    for (uint i = 0; i < self->map->nums_cols + 1; i++) self->size_tracker[i] = 0;
+    initialize_size_tracker(self);
     handle_row(self, row);
     free(row);
+    self->row_index++;
 
-    while (self->row_index < self->map->num_rows) {
+    for (; self->row_index < self->map->num_rows; self->row_index++) {
         row = self->map->getNextRow(self->map);
         handle_row(self, row);
         free(row);
     }
     return self->bsq;
+}
+
+void initialize_size_tracker(BsqFinder* self)
+{
+    self->size_tracker = malloc((self->map->nums_cols + 1) * sizeof (uint));
+    for (uint i = 0; i < self->map->nums_cols + 1; i++) {
+        self->size_tracker[i] = 0;
+    }
 }
 
 void handle_row(BsqFinder* self, char* row)
@@ -45,7 +54,7 @@ void handle_row(BsqFinder* self, char* row)
     for (uint j = 0; j < self->map->nums_cols; j++) {
         curr = self->size_tracker[j + 1];
         if (row[j] == FREE) {
-            self->size_tracker[j + 1] = min((int[]){prev, self->size_tracker[j], self->size_tracker[j + 1]}, 3) + 1;
+            self->size_tracker[j + 1] = min((int[]) {prev, self->size_tracker[j], self->size_tracker[j + 1]}, 3) + 1;
             if (self->size_tracker[j + 1] > self->bsq->size) {
                 self->bsq->setSize(self->bsq, self->size_tracker[j + 1]);
                 self->bsq->setBottomRight(self->bsq, self->row_index, j);
@@ -55,7 +64,6 @@ void handle_row(BsqFinder* self, char* row)
         }
         prev = curr;
     }
-    self->row_index++;
 }
 
 void delete(BsqFinder* self)
